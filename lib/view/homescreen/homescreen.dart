84d069a733d4334949ce3f38d2d092db7e4124e8
nyouibou <akshaits4@gmail.com>
2024-12-04 14:51:50 +0530

    updated carousel slider

--- lib/view/homescreen/homescreen.dart
+++ lib/view/homescreen/homescreen.dart
@@ -1,18 +1,52 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../controller/app_controller.dart';
 import '../widgets/catlist.dart';
-import 'categoryproduct.dart';
+// import 'categoryproduct.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<String> banners = [
+    "assets/img_5.png",
+    "assets/img_5.png",
+  ];
   final AppController controller = Get.put(AppController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Categories & Products'),
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () {
+              // Navigate to profile page
+            },
+            child: CircleAvatar(
+              backgroundColor: Colors.black,
+              radius: 25,
+            ),
+          ),
+        ),
+        title: Text("data"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Navigate to wallet page
+            },
+            icon: Icon(Icons.account_balance_wallet_outlined,
+                color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {
+              // Navigate to notifications page
+            },
+            icon: Icon(Icons.notifications_none_outlined, color: Colors.black),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -20,6 +54,30 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              CarouselSlider.builder(
+                itemCount: banners.length,
+                itemBuilder: (context, index, _) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(banners[index]),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  );
+                },
+                options: CarouselOptions(
+                  height: 162,
+                  viewportFraction: 0.8,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  enlargeCenterPage: true,
+                ),
+              ),
+
               Text("Top Categories"),
               // Categories Section
               Cataglist(controller: controller),
